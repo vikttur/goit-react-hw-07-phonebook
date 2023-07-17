aafbae2bd2e468a1be01c0133d8a66555dba998f
Viktor Biruk <vikttur.job@gmail.com>
2023-07-17 22:35:12 +0300

    removes work with the redux-persist library
    removes work with the LocalStorage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store, persistor} from '././redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from '././redux/store';
 import App from 'components/App/App';
 import './index.css';
 
@@ -10,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
         <App />
-      </PersistGate>
 		</Provider>
   </React.StrictMode>
 )
+
+// https://64b58d89f3dbab5a95c77653.mockapi.io/api/v1/contacts
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,10 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts';
 import { filterReducer } from './filter';
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filters: filterReducer,
-});
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['contacts'],
-};
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  devTools: true,
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {
+    contacts: contactsReducer,
+    filters: filterReducer,
+  },
 });
-export const persistor = persistStore(store);
